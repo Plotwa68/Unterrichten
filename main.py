61c99441f210206783c853f6d87436929bc050bf
Plotwa68 <125199727+Plotwa68@users.noreply.github.com>
2024-10-30 19:14:06 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+#1st program
+print((9**0.5)*5)
+
+#2nd program
+print((9.99 > 9.98)and(1000 != 1000.1))
+
+#3rd program
+print((2+2*2) == ((2+2)*2))
+
+
+#4th program
+S = '123.456'
+F = float(S) * 10
+I = int(F % 10)
+print(I)
