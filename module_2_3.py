69daec3169414f2f038e7d385d113c92c087738a
Plotwa68 <125199727+Plotwa68@users.noreply.github.com>
2024-11-19 19:15:52 +0300

    Update module_2_3.py

--- module_2_3.py
+++ module_2_3.py
@@ -1,17 +1,15 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 
 result = True
-
+i = 0
 while result:
-    number = int(input('Введите целое неотрицательное цисло: '))
 
-    if isinstance(number, int) and number <= len(my_list) and number > 0:
+    if my_list[i] < 0:
         result = False
-        while number != 0:
-            number -= 1
-            print(my_list[number])
     else:
-        continue
+        print(my_list[i])
+
+    i += 1
 
 
 
