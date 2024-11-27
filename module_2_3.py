47bf49beed6cb4dea49ad091c77c3b6a893f1079
Plotwa68 <125199727+Plotwa68@users.noreply.github.com>
2024-11-27 21:47:18 +0300

    Update module_2_3.py

--- module_2_3.py
+++ module_2_3.py
@@ -1,16 +1,17 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 
-i = 0
-while True:
+#my_list = [42, 69, 322, 13, 0, 99, 5, 9, 8, 7, 6, 5]
 
-    if my_list[i] < 0:
+index_elem = 0
+while index_elem < len(my_list):
+    elem = my_list[index_elem]
+    index_elem += 1
+    if elem == 0:
+        continue
+    elif elem < 0:
         break
-
-    elif my_list[i] != 0:
-        print(my_list[i])
-
-    i += 1
-
+    else:
+        print(elem)
 
 
 
